8b3fc7c9517285839c48c7c7e8a4aced37f7c99f
ovidionavarro <ovidionavarro90@gmail.com>
2025-07-03 22:13:14 -0400

    fix footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,17 +4,6 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
         <p className="text-sm">&copy; {new Date().getFullYear()} TuEmpresaYates. Todos los derechos reservados.</p>
         
-        {/* <div className="flex space-x-5 text-xl">
-          <a href="https://facebook.com/tuempresa" target="_blank" rel="noreferrer" className="hover:text-yellow-400 transition">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="https://instagram.com/tuempresa" target="_blank" rel="noreferrer" className="hover:text-yellow-400 transition">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="https://twitter.com/tuempresa" target="_blank" rel="noreferrer" className="hover:text-yellow-400 transition">
-            <i className="fab fa-x-twitter"></i>
-          </a>
-        </div> */}
       </div>
     </footer>
   );
